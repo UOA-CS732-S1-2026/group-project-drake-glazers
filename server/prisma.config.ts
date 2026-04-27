752d57d3d9bfdb5c6d3a4e6479166169de476095
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2026-04-27 17:23:34 +1200

    chore(memories-endpoint): prisma config direct URL needed to be fixed

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   },
   datasource: {
     url: env('DATABASE_URL'),
-    shadowDatabaseUrl: env('DIRECT_URL'),
+    ...(process.env.DIRECT_URL ? { shadowDatabaseUrl: process.env.DIRECT_URL } : {}),
   },
 });
