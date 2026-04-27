751cb551084759e252c5244d5c5ca4b5aa991e29
Dave <dkha055@aucklanduni.ac.nz>
2026-04-28 09:28:24 +1200

    chore: added redirect when a user isn't logged into clerk

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,16 +1,40 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import { useAuth } from '@clerk/clerk-expo';
+
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect } from 'react';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   anchor: '(nav)',
 };
 
 export default function RootLayout() {
+
+  const {isSignedIn, isLoaded} = useAuth();
+  
+  useEffect(() => {
+
+    if (!isLoaded) return;
+
+    SplashScreen.hideAsync();
+
+    if (!isSignedIn) {
+      router.replace('/(auth)/sign-in');
+    }
+  
+  }, [isLoaded, isSignedIn])
+
   const colorScheme = useColorScheme();
+
+  if (!isLoaded) return null;
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
