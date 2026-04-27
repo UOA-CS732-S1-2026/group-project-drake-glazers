8be98fa2c360b0b9bb9a51e7491d8685b9ca27df
Dave <dkha055@aucklanduni.ac.nz>
2026-04-28 08:34:24 +1200

    chore: Create files for each item in the navbar

---
+++ client/app/(nav)/explore.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+
+export default function ExploreScreen() {
+  return (
+    <View>
+    </View>
+  );
+}
+
+
---
+++ client/app/(nav)/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+
+
+// This is the root of the application, it contains the map.
+export default function HomeScreen() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+
