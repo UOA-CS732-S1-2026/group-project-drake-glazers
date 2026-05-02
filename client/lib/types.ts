0dbe74945abbb52f412a712d5a662087273513b4
Dave <dkha055@aucklanduni.ac.nz>
2026-05-02 17:29:15 +1200

    feat: implemented memory flow with button that leads to a form

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -5,13 +5,26 @@ export type User = {
   updatedAt: string;
 };
 
+export type Visibility = 'public' | 'friends_only' | 'private';
+
 export type Memory = {
   id: string;
   userId: string;
   title: string;
   latitude: number;
   longitude: number;
-  visibility: 'public' | 'private';
+  visibility: Visibility;
+  description?: string;
   createdAt: string;
   updatedAt: string;
+};
+
+export type MediaType = 'IMAGE' | 'VIDEO' | 'VOICE_NOTE';
+
+export type Media = {
+  id: string;
+  memoryId: string;
+  mediaPath: string;
+  mediaType: MediaType;
+  signedUrl?: string;
 };
